f78de982c22e3b7f8df6b6d61fa2fbcd4dc64d05
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2022-06-11 20:57:27 +0530

    Add files via upload

---
+++ prime_number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter n:- "<<endl;
+    cin>>n;
+
+    int d=2;
+    bool divided = false;
+    while(d<n){
+        if(n%d == 0){
+            cout<<"Not Prime"<<endl;
+            divided = true;
+        }
+        d=d+1;
+    }
+    if(!divided){
+        cout<<"prime"<<endl;
+    }
+
+    return 0;
+}
---
+++ whileLoop.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	// Your code goes here;
+	int n;
+	cout<<"Enter n: "<<endl;
+	cin>>n;
+	
+	int i=1;
+	while(i<=n){
+		cout<<i<<endl;
+		i=i+1;
+	}
+
+	return 0;
+}
